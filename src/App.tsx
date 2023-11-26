62b92236f2523e72c391ba65708871f3bbbed92c
Ryan Rodriguez <rjoe.rod@gmail.com>
2023-11-25 20:07:26 -0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
+
+function App() {
+	return (
+		<HashRouter basename="/">
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+			<hr />
+			<Routes>
+				<Route path="/" Component={Home} />
+				<Route path="/about" Component={About} />
+			</Routes>
+		</HashRouter>
+	);
+}
+
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+);
+
+export default App;
